625e3387fbf8c4f0ce6e99957e4e4f4f7791fbed
archit1210 <architlokhande72@gmail.com>
2025-01-29 00:54:43 +0530

    Basic form added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <Navbar/>
+  <TaskForm/>
   <h1>This is app component</h1>
 </template>
 <script setup>
 import Navbar from './components/Navbar.vue';
+import TaskForm from './components/TaskForm.vue';
 </script>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap');
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <form @submit.prevent="addTask">
+      <label for="">Add your task  :     </label>
+      <input type="text" placeholder="Enter a task" v-model="task" />
+      <button type="submit">ADD</button>
+    </form>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">{{ task }}</li>
+    </ul>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  // Reactive variables
+  const task = ref('');
+  const tasks = ref([]);
+  
+  // Function to add task
+  const addTask = () => {
+    if (task.value.trim()) {
+      tasks.value.push(task.value.trim());
+      task.value = ''; // Clear input after adding
+    }
+  };
+  </script>
+  
