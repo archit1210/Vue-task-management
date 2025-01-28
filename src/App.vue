cfd377b7e3c66e10b2ceb4b01bb60fd988776b3e
archit1210 <architlokhande72@gmail.com>
2025-01-29 00:27:25 +0530

    Google font added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <h1>This is app component</h1>
 </template>
-<script setup></script>
-<style scoped></style>
+<script setup>
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap');
+body{
+  font-family: "Figtree", serif;;
+}
+</style>
+
