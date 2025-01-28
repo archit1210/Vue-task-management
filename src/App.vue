163b9f398faf100be50b714388238909d5fdf6c4
archit1210 <architlokhande72@gmail.com>
2025-01-29 00:41:12 +0530

    Navbar component added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
+  <Navbar/>
   <h1>This is app component</h1>
 </template>
 <script setup>
+import Navbar from './components/Navbar.vue';
 </script>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap');
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 body{
   font-family: "Figtree", serif;;
 }
---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="parent">
+        <div class="navbar-routes">Home</div>
+        <div class="navbar-routes">Completed</div>
+        <div class="navbar-routes">Pending</div>
+        <div class="navbar-routes">In Progress</div>
+    </div>
+</template>
+
+<script setup></script>
+<style scoped>
+.parent{
+    height:5rem;
+    width: 100vw;
+    display: flex;
+    gap: 2rem;
+    justify-content: center;
+    align-items: center;
+    background: rgb(2,0,36);
+    background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(241,92,92,1) 0%, rgba(0,212,255,1) 100%);
+}
+
+.navbar-routes{
+    color: #ffffff;
+    font-weight: 700;
+    cursor: pointer;
+    font-size: 1.3rem;
+}
+</style>
